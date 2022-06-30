1e0edb7eb7ff5d4fa8a15619d21f21e61bf65fbc
Joseth Ariel <arielbaquero.ab@gmail.com>
2022-06-30 15:21:20 -0500

    first commit

---
+++ src/main/java/software/ariel/registrationloginbackend/RegistrationLoginBackendApplication.java
@@ -0,0 +1,13 @@
+package software.ariel.registrationloginbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationLoginBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/software/ariel/registrationloginbackend/RegistrationLoginBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package software.ariel.registrationloginbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
